aaf284e948c4832413c2698bf145a924e87eb556
Evgeniy Savenko <wayrez27@gmail.com>
2022-10-03 23:11:14 +0300

    feat: added functional for download files and accordeon

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Forms from './modules/forms';
+import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -46,4 +48,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const forms = new Forms('.form');
     forms.init();
+
+    new ShowInfo('.plus__content').init();
+
+    new Download('.download').init();
 });
